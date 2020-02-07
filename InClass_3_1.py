53a5e5a049f8cef6a472e8736d9351b846a073f8
mayawhaley <mawhaley430@gmail.com>
2020-02-07 03:38:38 +0000

    Adding required files and app

---
+++ InClass_3_1.py
@@ -0,0 +1,16 @@
+import flask
+import os
+
+app = flask.Flask(__name__)
+
+
+@app.route('/') # Python decorator 
+def index(): 
+    print("I'm debugging!")
+    return "Hello, Maya!"
+
+app.run(
+    port=int(os.getenv('PORT',8080)),
+    host=os.getenv('IP', '0.0.0.0'),
+    debug=True
+    )
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python InClass_3_1.py
